5a76e1b8907d3705ed388b2ec1e10b5377ec5b7c
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-09-07 18:42:19 +0530

    Update 3.Uncommon characters.py

--- Dictionary/3.Uncommon characters.py
+++ Dictionary/3.Uncommon characters.py
@@ -30,3 +30,37 @@ class Solution:
         
     
     return str
+
+
+
+#---OR------
+
+class Solution:
+    # Function to find uncommon characters between two strings.
+  def uncommonChars(self, s1, s2):
+    
+    #The ord() function in Python is a built-in function 
+    #that returns the Unicode code point of a single character. 
+    
+    #enumerate() is a built-in function that adds a counter (index) to an iterable (like a list, tuple, or string). 
+    #It’s commonly used in for loops when you need both the index and the value of the items you're looping through.
+   
+    freq = [0]*26
+    ans=set()
+    
+    for e in s1:
+        freq[ord(e)-ord('a')]=1
+    
+    for e in s2:
+        if freq[ord(e)-ord('a')]==1 or freq[ord(e)-ord('a')]==-1:
+            freq[ord(e)-ord('a')]=-1 #present in both marked as -1
+        else:
+            freq[ord(e)-ord('a')]=2
+    
+    res=""
+    
+    for index,value in enumerate(freq):
+        if value==1 or value==2:
+            res+=chr(index+ord('a')) #the chr() function returns the character (a string) corresponding to an ASCII or Unicode
+    
+    return res

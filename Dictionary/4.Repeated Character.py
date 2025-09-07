5ca0ec8c55951f19cb35611648288a59ac9f2fba
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-09-07 19:13:09 +0530

    Create 4.Repeated Character.py

---
+++ Dictionary/4.Repeated Character.py
@@ -0,0 +1,36 @@
+#https://www.geeksforgeeks.org/problems/repeated-character2058/1?page=1&category=Hash,Map&difficulty=Basic&sortBy=submissions&selectedLang=python3
+
+class Solution:
+    def firstRep(self, s):
+        
+        pos={}
+        str=set()
+        
+        for index,char in enumerate(s):
+            if pos.get(char)==None:
+                pos[char]=index
+            else:
+                str.add(char)
+        
+        ans=""
+        min=float('inf')
+        for i in str:
+            if i in pos:
+                if pos[i]<min:
+                    min=pos[i]
+                    ans=i
+        
+        if min==float('inf'):
+            return -1
+        else:
+            return ans
+            
+#float('inf')
+#This represents infinity, which is guaranteed to be greater 
+#than any integer value in the string indices.
+                    
+        
+                
+                
+
+            

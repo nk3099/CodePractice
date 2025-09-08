796e5fa1f8d058c6f7ad220aaf14368de6320ce0
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-09-08 20:42:05 +0530

    Update 12. Longest Subarray with Sum K.py

--- Dictionary/12. Longest Subarray with Sum K.py
+++ Dictionary/12. Longest Subarray with Sum K.py
@@ -18,5 +18,29 @@ class Solution:
         """
             
         
-        #Optimized O(N) Solution Using Prefix Sum + Hash Map
+        #Optimized O(N) Solution Using Prefix Sum + Hash Map 
+        #YT TakeUForward: https://www.youtube.com/watch?v=frf7qxiN2qU
+
         
+        hashmap={}
+        prefixsum=0
+        l=0
+        for i in range(len(arr)):
+            prefixsum+=arr[i]
+            if prefixsum==k:
+                l=max(l,i+1)
+            
+            rem=prefixsum-k
+            if rem in hashmap:
+                l=max(l,i-hashmap[rem])
+            
+            #hashmap[prefixsum]=i
+            #But, should only set hashmap[sum] if sum is not already in hashmap, 
+            #to keep the earliest index for that prefix sum. (consider [2,0,0,0,3])
+             
+            if prefixsum not in hashmap:
+                hashmap[prefixsum]=i
+            
+            
+            
+        return l

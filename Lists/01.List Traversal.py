cff6374d7ee58abc249ff61fecd4a582fd365f7e
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-09-09 21:21:34 +0530

    Create 01.List Traversal.py

---
+++ Lists/01.List Traversal.py
@@ -0,0 +1,11 @@
+#https://www.geeksforgeeks.org/problems/list-traversal/1&selectedLang=python3
+
+"""
+print(i, end=" "): prints each element followed by a space instead of a newline.
+
+print() at the end (optional): adds a newline after all elements are printed, so the terminal prompt doesn’t stay on the same line.
+"""
+
+def listTraversal(arr):
+    for i in arr:
+        print(i,end=" ")
